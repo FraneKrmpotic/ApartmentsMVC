84e291ef8176e69bdce49c3c2ae9e65aea5c7347
Frane Krmpotić <franekrmpotic@gmail.com>
2022-09-09 00:05:57 +0200

    commit

---
+++ Apartments/Apartments/Controllers/ApartmentsController.cs
@@ -0,0 +1,169 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Data.Entity;
+
+namespace Apartments.Controllers
+{
+    public class ApartmentsController : Controller
+    {
+        ~ApartmentsController()
+        {
+            if (db != null)
+            {
+                db.Dispose();
+            }
+        }
+        private readonly ModelContainer db = new ModelContainer();
+        // GET: Apartments
+        public ActionResult Index()
+        {
+            return View(db.Apartments);
+        }
+
+        // GET: Apartments/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(System.Net.HttpStatusCode.BadRequest);
+            }
+            Apartment apartment = db.Apartments
+                .Include(a => a.UploadedFiles)
+                .SingleOrDefault(a => a.IDApartment == id);
+            if (apartment == null)
+            {
+                return HttpNotFound();
+            }
+            return View(apartment);
+        }
+
+        // GET: Apartments/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Apartments/Create
+        [HttpPost]
+        public ActionResult Create([Bind(Include = "Address, City, Contact")] Apartment apartment, 
+            IEnumerable<HttpPostedFileBase> files)
+        {
+            if (ModelState.IsValid)
+            {
+                apartment.UploadedFiles = new List<UploadedFile>();
+                foreach (var file in files)
+                {
+                    if (file != null && file.ContentLength > 0)
+                    {
+                        var picture = new UploadedFile
+                        {
+                            Name = System.IO.Path.GetFileName(file.FileName),
+                            ContentType = file.ContentType
+                        };
+                        using (var reader = new System.IO.BinaryReader(file.InputStream))
+                        {
+                            picture.Content = reader.ReadBytes(file.ContentLength);
+                        }
+                        apartment.UploadedFiles.Add(picture);
+                    }
+                }
+
+                db.Apartments.Add(apartment);
+                db.SaveChanges();
+            }
+
+            return RedirectToAction("Index");
+
+        }
+
+        // GET: Apartments/Edit/5
+        public ActionResult Edit(int? id)
+        {
+
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(System.Net.HttpStatusCode.BadRequest);
+            }
+            Apartment apartment = db.Apartments
+                .Include(a => a.UploadedFiles)
+                .SingleOrDefault(a => a.IDApartment == id);
+            if (apartment == null)
+            {
+                return HttpNotFound();
+            }
+            return View(apartment);
+        }
+
+        // POST: Apartments/Edit/5
+        [HttpPost]
+        [ActionName("Edit")]
+        public ActionResult EditConfirmed(int id, IEnumerable<HttpPostedFileBase> files)
+        {
+            Apartment apartmentToUpdate = db.Apartments.Find(id);
+            if (TryUpdateModel(apartmentToUpdate, "", new string[] { "Address", "City", "Contact" }))
+            {
+
+                if (apartmentToUpdate.UploadedFiles == null)
+                {
+                    apartmentToUpdate.UploadedFiles = new List<UploadedFile>(); 
+                }
+                foreach (var file in files)
+                {
+                    if (file != null && file.ContentLength > 0)
+                    {
+                        var picture = new UploadedFile
+                        {
+                            Name = System.IO.Path.GetFileName(file.FileName),
+                            ContentType = file.ContentType
+                        };
+                        using (var reader = new System.IO.BinaryReader(file.InputStream))
+                        {
+                            picture.Content = reader.ReadBytes(file.ContentLength);
+                        }
+                        apartmentToUpdate.UploadedFiles.Add(picture);
+                    }
+                }
+
+
+
+
+                db.Entry(apartmentToUpdate).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(apartmentToUpdate);
+        }
+
+        // GET: Apartments/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(System.Net.HttpStatusCode.BadRequest);
+            }
+            Apartment apartment = db.Apartments
+                .Include(a => a.UploadedFiles)
+                .SingleOrDefault(a => a.IDApartment == id);
+            if (apartment == null)
+            {
+                return HttpNotFound();
+            }
+            return View(apartment);
+        }
+
+        // POST: Apartments/Delete/5
+        [HttpPost]
+        [ActionName("Delete")]
+        public ActionResult DeleteConfirmed(int id, FormCollection collection)
+        {
+            db.UploadedFiles.RemoveRange(db.UploadedFiles.Where(f => f.ApartmentIDApartment == id));
+            db.Apartments.Remove(db.Apartments.Find(id));
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+    }
+}
---
+++ Apartments/Apartments/Controllers/OwnerController.cs
@@ -0,0 +1,119 @@
+﻿using Antlr.Runtime.Misc;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Apartments.Controllers
+{
+    public class OwnerController : Controller
+    {
+        ~OwnerController()
+        {
+            if (db != null)
+            {
+                db.Dispose();
+            }
+        }
+        private readonly ModelContainer db = new ModelContainer();
+
+        // GET: Owner
+        public ActionResult OwnerIndex()
+        {
+            return View(db.Owners);
+        }
+
+        // GET: Owner/Details/5
+        public ActionResult DetailsOwner(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(System.Net.HttpStatusCode.BadRequest);
+            }
+            Owner owner = db.Owners.SingleOrDefault(a => a.IDOwner == id);
+            if (owner == null)
+            {
+                return HttpNotFound();
+            }
+            return View(owner);
+        }
+
+        // GET: Owner/Create
+        public ActionResult CreateOwner()
+        {
+            return View();
+        }
+
+        // POST: Owner/Create
+        [HttpPost]
+        public ActionResult Create([Bind(Include = "FirstName, LastName, ApartmentIDApartment")] Owner owner)
+        {
+
+            if (ModelState.IsValid)
+            {
+                db.Owners.Add(owner);
+                db.SaveChanges();
+            }
+            return RedirectToAction("OwnerIndex");
+        }
+
+        // GET: Owner/Edit/5
+        public ActionResult EditOwner(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(System.Net.HttpStatusCode.BadRequest);
+            }
+            Owner owner = db.Owners
+                .SingleOrDefault(a => a.IDOwner == id);
+            if (owner == null)
+            {
+                return HttpNotFound();
+            }
+            return View(owner);
+        }
+
+        // POST: Owner/Edit/5
+        [HttpPost]
+        [ActionName("Edit")]
+        public ActionResult EditConfirmed()
+        {
+            Owner apartmentToUpdate = db.Owners.FirstOrDefault();
+            if (TryUpdateModel(apartmentToUpdate, "",
+                new string[] { "FirstName", "LastName", "ApartmentIDApartment" }))
+            {
+                db.Entry(apartmentToUpdate).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("OwnerIndex");
+            }
+            return View(apartmentToUpdate);
+        }
+
+        // GET: Owner/Delete/5
+        public ActionResult DeleteOwner(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(System.Net.HttpStatusCode.BadRequest);
+            }
+            Owner owner = db.Owners.SingleOrDefault(a => a.IDOwner == id);
+            if (owner == null)
+            {
+                return HttpNotFound();
+            }
+            return View(owner);
+        }
+
+        // POST: Owner/Delete/5
+        [HttpPost]
+        [ActionName("DeleteOwner")]
+        public ActionResult Delete(int id, FormCollection collection)
+        {
+            db.Owners.Remove(db.Owners.Find(id));
+            db.SaveChanges();
+            return RedirectToAction("OwnerIndex");
+        }
+    }
+}
